d1aab847afc7f64e216ffa948e86112d65417e32
Pap-Ski <niilaryeaquarteypapafio@gmail.com>
2021-09-01 20:07:34 +0000

    added audio files in layout

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// let whiteKeys = document.querySelectorAll(".white__key")
+let allKeys = document.querySelectorAll(".keys > div");
+
+allKeys.forEach((key) => key.addEventListener("mouseover", playNote));
+
+function playNote(e) {
+  let audio = this.children[0];
+  audio.currentTime = 0;
+  audio.play();
+}
