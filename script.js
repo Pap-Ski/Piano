27204544874e64a09df804939d1d0067b4e9de88
Pap-Ski <niilaryeaquarteypapafio@gmail.com>
2021-09-02 15:59:11 +0000

    added keyboard and mouse events

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 let allKeys = document.querySelectorAll(".keys > div");
 
-allKeys.forEach((key) => key.addEventListener("mouseover", playNote));
+allKeys.forEach((key) => key.addEventListener("click", playNoteOnClick));
 
-function playNote(e) {
+function playNoteOnClick(e) {
   let audio = this.children[0];
   audio.currentTime = 0;
   audio.play();
+
+  animateKey(this);
+}
+
+document.addEventListener("keydown", playNoteOnKeyDown);
+function playNoteOnKeyDown(e) {
+  if (e.repeat) return;
+  allKeys.forEach((key) => {
+    let audio = key.children[0];
+
+    if (e.key === key.getAttribute("data-key")) {
+      audio.currentTime = 0;
+      audio.play();
+      animateKey(key);
+    }
+  });
+}
+
+function animateKey(key) {
+  key.classList.add("press");
+  key.addEventListener("transitionend", () => {
+    key.classList.remove("press");
+  });
 }
